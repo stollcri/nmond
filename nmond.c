93d6831d24eb61c16b0347bdd337aa844daca508
Christopher Stoll <stollcri@gmail.com>
2015-10-04 16:36:38 -0400

    debug code

--- nmond.c
+++ nmond.c
@@ -309,11 +309,30 @@ int main(int argc, char **argv)
 	// getsyshwinfo(&tmphw);
 	// printf("%lld\n", tmphw.memorysize);
 	// 
+	// 
 	// struct sysres tmpres = SYSRES_INIT;
 	// getsysresinfo(&tmpres);
 	// size_t tmpproccnt = 0;
-	// struct sysproc *tmprpoc = NULL;
+	// struct sysproc **tmprpoc = NULL;
 	// struct hashitem *tmphash = hashtnew();
+	// printf("0 ... %8x \n", (unsigned int)tmprpoc);
+	// tmprpoc = getsysprocinfoall(&tmpproccnt, tmprpoc, &tmphash, tmpres.percentallcpu, &tmpres);
+	// printf("  ... %8x \n", (unsigned int)tmprpoc);
+	// printf("1 ... \n");
+	// sleep(1);
+	// getsysprocinfoall(&tmpproccnt, tmprpoc, &tmphash, tmpres.percentallcpu, &tmpres);
+	// printf("2 ... \n");
+	
+	// sleep(1);
+	// getsysprocinfoall(&tmpproccnt, &tmprpoc, &tmphash, tmpres.percentallcpu, &tmpres);
+	// printf("3 ... \n");
+	// sleep(1);
+	// getsysprocinfoall(&tmpproccnt, &tmprpoc, &tmphash, tmpres.percentallcpu, &tmpres);
+	// printf("4 ... \n");
+	// sleep(1);
+	// getsysprocinfoall(&tmpproccnt, &tmprpoc, &tmphash, tmpres.percentallcpu, &tmpres);
+	// printf("5 ... \n");
+	// 
 	// cbreak();
 	// timeout(20);
 	// int inp = 0;
@@ -327,6 +346,7 @@ int main(int argc, char **argv)
 	// struct uiwins tmpwins = UIWINS_INIT;
 	// struct nmondstate tmpcurrentstate = NMONDSTATE_INIT;
 	// processenvars(&tmpwins, &tmpcurrentstate);
+	// 
 	// exitapp();
 
 
