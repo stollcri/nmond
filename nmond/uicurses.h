1815ac235c5b2de9e94bf429ebcaf59898b4e957
Christopher Stoll <stollcri@gmail.com>
2015-10-19 21:41:34 -0400

    v0.1.12

--- nmond/uicurses.h
+++ nmond/uicurses.h
@@ -44,8 +44,8 @@
 #include <sys/time.h>
 
 #define APPNAME "nmond"
-#define VERSION "0.1.11"
-#define VERDATE "2015-10-18"
+#define VERSION "0.1.12"
+#define VERDATE "2015-10-19"
 #define APPURL "https://github.com/stollcri/nmond"
 
 #define MAXROWS 256
